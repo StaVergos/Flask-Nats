99c3c67385eaecbb598dfc96cff083f73e2c0e1d
Stavros Vergos <stavrospvergos@gmail.com>
2022-10-20 10:17:11 +0300

    Chore: Removed redundant msg handler function

--- services/nats_req_rep.py
+++ services/nats_req_rep.py
@@ -7,13 +7,6 @@ from nats.errors import ConnectionClosedError, NoServersError, TimeoutError
 async def main():
     nc = await nats.connect("0.0.0.0:4222")
 
-    async def message_handler(msg):
-        subject = msg.subject
-        reply = msg.reply
-        data = msg.data.decode()
-        print("Received a message on '{subject} {reply}': {data}".format(
-            subject=subject, reply=reply, data=data))
-
     try:
         response = await nc.request("help", b'help me', timeout=0.5)
         print("Received response: {message}".format(
